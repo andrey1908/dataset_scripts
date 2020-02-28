8946f00dd9377cb49a403d3b694306a2905656a5
andrey1908 <andrey.krishtopik@mail.ru>
2020-02-28 19:25:33 +0300

    remove_small_boxes_scale.py: new keys width and height

--- remove_small_boxes_scale.py
+++ remove_small_boxes_scale.py
@@ -5,7 +5,9 @@ import json
 def build_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-json', '--json-file', required=True, type=str)
-    parser.add_argument('-thr', '--threshold', required=True, type=float)  # 55
+    parser.add_argument('-thr', '--threshold', required=True, type=float)
+    parser.add_argument('-w', '--width', required=True, type=float)
+    parser.add_argument('-ht', '--height', required=True, type=float)
     parser.add_argument('-out', '--out-file', required=True, type=str)
     return parser
 
@@ -17,11 +19,11 @@ def get_image_id_to_image(json_dict):
     return image_id_to_image
 
 
-def remove_small_boxes_scale(json_dict, threshold, image_id_to_image):
+def remove_small_boxes_scale(json_dict, threshold, width, height, image_id_to_image):
     sub = 0
     for i in range(len(json_dict['annotations'])):
         image = image_id_to_image[json_dict['annotations'][i - sub]['image_id']]
-        scale = min(640/image['width'], 384/image['height'])
+        scale = min(width/image['width'], height/image['height'])
         if json_dict['annotations'][i - sub]['area'] * scale * scale < threshold:
             del json_dict['annotations'][i - sub]
             sub += 1
@@ -36,7 +38,7 @@ if __name__ == '__main__':
     with open(args.json_file, 'r') as f:
         json_dict = json.load(f)
     image_id_to_image = get_image_id_to_image(json_dict)
-    removed = remove_small_boxes_scale(json_dict, args.threshold, image_id_to_image)
+    removed = remove_small_boxes_scale(json_dict, args.threshold, args.width, args.height, image_id_to_image)
     print('Removed {} boxes'.format(removed))
     with open(args.out_file, 'w') as f:
         json.dump(json_dict, f, indent=2)
