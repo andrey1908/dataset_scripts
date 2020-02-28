a82d38d43c84455cda0700774a5879daeba3a4be
andrey1908 <andrey.krishtopik@mail.ru>
2020-02-28 22:19:29 +0300

    replace_classes.py: upgrade

--- replace_classes.py
+++ replace_classes.py
@@ -1,26 +1,37 @@
 import argparse
 import json
 from reindex_json import reindex_json
+import re
 
 
 def build_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-json', '--json-file', required=True, type=str)
+    parser.add_argument('-new-cats', '--new-categories-names', required=True, type=str, nargs='+')
+    parser.add_argument('-old-cat-name-to-new', '--old-category-name-to-new', required=True, type=str)
     parser.add_argument('-out', '--out-file', required=True, type=str)
     return parser
 
 
-def replace_categories(categories):
-    new_categories = [{"supercategory": "none", "id": 1, "name": "traffic_sign"},
-                      {"supercategory": "none", "id": 2, "name": "car"},
-                      {"supercategory": "none", "id": 3, "name": "person"},
-                      {"supercategory": "none", "id": 4, "name": "traffic_light"}]
-    old_category_name_to_new_id = {'1.22': 1, '1.23': 1, '2.4': 1, '2.5': 1, '3.1': 1, '3.2': 1, '3.24': 1, '3.4': 1, '5.19': 1, '3.25+3.31': 1, 'car': 2, 'other': 1, 'person': 3, 'stop_sign': 1, 'traffic_light': 4, 'truck': 2}
+def get_new_categories(new_categories_names):
+    new_categories = list()
+    new_category_name_to_id = dict()
+    cat_id = 1
+    for new_cat_name in new_categories_names:
+        new_cat = {"supercategory": "none", "id": cat_id, "name": new_cat_name}
+        new_categories.append(new_cat)
+        new_category_name_to_id[new_cat_name] = cat_id
+        cat_id += 1
+    return new_categories, new_category_name_to_id
+
+
+def replace_categories(old_categories, new_categories_names, old_category_name_to_new):
+    new_categories, new_category_name_to_id = get_new_categories(new_categories_names)
     old_category_id_to_new = dict()
-    for old_category in categories:
-        if old_category['name'] in old_category_name_to_new_id.keys():
-            old_category_id_to_new[old_category['id']] = old_category_name_to_new_id[old_category['name']]
-    categories[:] = new_categories
+    for old_category in old_categories:
+        if old_category['name'] in old_category_name_to_new.keys():
+            old_category_id_to_new[old_category['id']] = new_category_name_to_id[old_category_name_to_new[old_category['name']]]
+    old_categories[:] = new_categories
     return old_category_id_to_new
 
 
@@ -32,10 +43,19 @@ def correct_annotations(annotations, old_category_id_to_new):
         annotations[i]['category_id'] = old_category_id_to_new[annotations[i]['category_id']]
 
 
-def replace_classes(json_dict):
-    old_category_id_to_new = replace_categories(json_dict['categories'])
+def replace_classes(json_dict, new_categories_names, old_category_name_to_new):
+    old_category_id_to_new = replace_categories(json_dict['categories'], new_categories_names, old_category_name_to_new)
     correct_annotations(json_dict['annotations'], old_category_id_to_new)
     reindex_json(json_dict)
+
+
+def parse_old_category_name_to_new(args_old_category_name_to_new):
+    old_category_name_to_new = dict()
+    args_old_category_name_to_new = args_old_category_name_to_new.split(' ')
+    for one_convertion in args_old_category_name_to_new:
+        old_name, new_name = re.split('->', one_convertion)
+        old_category_name_to_new[old_name] = new_name
+    return old_category_name_to_new
 
 
 if __name__ == '__main__':
@@ -43,7 +63,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
     with open(args.json_file, 'r') as f:
         json_dict = json.load(f)
-    replace_classes(json_dict)
+    old_category_name_to_new = parse_old_category_name_to_new(args.old_category_name_to_new)
+    replace_classes(json_dict, args.new_categories_names, old_category_name_to_new)
     with open(args.out_file, 'w') as f:
         json.dump(json_dict, f, indent=2)
 
