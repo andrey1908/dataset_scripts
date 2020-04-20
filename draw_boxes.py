1057e5949e6a82645238e2ff5defa6a1ae7727eb
andrey1908 <andrey.krishtopik@mail.ru>
2020-04-20 17:00:34 +0300

    new flags --images-files-to-draw and --only-with-boxes

--- draw_boxes.py
+++ draw_boxes.py
@@ -13,9 +13,10 @@ def build_parser():
     parser.add_argument('-json', '--json-file', required=True, type=str)
     parser.add_argument('-img-fld', '--images-folder', required=True, type=str)
     parser.add_argument('-out-fld', '--out-folder', required=True, type=str)
-    parser.add_argument('-draw-image', '--draw-single-image', type=str)
+    parser.add_argument('-imgs-to-draw', '--images-files-to-draw', type=str, nargs='+')
     parser.add_argument('-num', '--images-number', type=int)
     parser.add_argument('-rnd', '--random', action='store_true')
+    parser.add_argument('-owb', '--only-with-boxes', action='store_true')
     parser.add_argument('-img-json', '--images-json-file', type=str)
     parser.add_argument('-preserve-files-tree', '--preserve-files-tree', action='store_true')
     parser.add_argument('-thr', '--threshold', type=float, default=0.)
@@ -66,12 +67,32 @@ def preprocess_box(box, im_w, im_h):
     box[:] = [round(b) for b in box]
 
 
-def get_images_to_draw(images, images_folder, draw_single_image=None, images_number=None, random=False):
-    if draw_single_image:
-        image_name = os.path.relpath(draw_single_image, images_folder)
-        images_to_draw = [find_image_by_name(images, image_name)]
+def get_images_idxs_with_boxes(json_dict, threshold=0.):
+    image_id_to_idx = dict()
+    for i, image in enumerate(json_dict['images']):
+        image_id_to_idx[image['id']] = i
+    images_idxs_with_boxes = set()
+    for ann in json_dict['annotations']:
+        if ann.get('score', 999.) >= threshold:
+            images_idxs_with_boxes.add(image_id_to_idx[ann['image_id']])
+    return list(images_idxs_with_boxes)
+
+
+def get_images_to_draw(json_dict, images_folder, images_files_to_draw=None, images_number=None, random=False, only_with_boxes=False,
+                       threshold=0.):
+    images = json_dict['images']
+    if images_files_to_draw:
+        images_to_draw = list()
+        for image_file_to_draw in images_files_to_draw:
+            image_name = os.path.relpath(image_file_to_draw, images_folder)
+            images_to_draw.append(find_image_by_name(images, image_name))
     elif images_number:
-        images_idxs = [i for i in range(len(images))]
+        if only_with_boxes:
+            images_idxs = get_images_idxs_with_boxes(json_dict, threshold)
+        else:
+            images_idxs = list(range(len(images)))
+        if images_number > len(images_idxs):
+            raise RuntimeError('There aren\'t so many images to draw on. Maximum {} images.'.format(len(images_idxs)))
         if random:
             np.random.shuffle(images_idxs)
         images_to_draw = list()
@@ -82,12 +103,13 @@ def get_images_to_draw(images, images_folder, draw_single_image=None, images_num
     return images_to_draw
 
 
-def draw_boxes(json_dict, images_folder, out_folder, draw_single_image=None, images_number=None, random=False,
-               preserve_files_tree=False, threshold=0.):
+def draw_boxes(json_dict, images_folder, out_folder, images_files_to_draw=None, images_number=None, random=False,
+               only_with_boxes=False, preserve_files_tree=False, threshold=0.):
     Path(out_folder).mkdir(parents=True, exist_ok=True)
     image_id_to_annotations_idxs = get_image_id_to_annotations_idxs(json_dict['images'], json_dict['annotations'])
     category_id_to_name = get_category_id_to_name(json_dict['categories'])
-    images_to_draw = get_images_to_draw(json_dict['images'], images_folder, draw_single_image, images_number, random)
+    images_to_draw = get_images_to_draw(json_dict, images_folder, images_files_to_draw, images_number, random, only_with_boxes,
+                                        threshold)
     if images_to_draw is None:
         raise RuntimeError()
     uniquer = UniquePathsNamesGenerator()
@@ -112,7 +134,9 @@ def draw_boxes(json_dict, images_folder, out_folder, draw_single_image=None, ima
             im_draw.rectangle(box, outline='red', width=width)
             font_size = int(max(im.size)/100) + 10
             font = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', font_size)
-            im_draw.text((box[0], box[1]-font_size), cat_name + score_str, fill='red', font=font)
+            text_x = min(box[0], im.width-font.getsize(cat_name + score_str)[0])
+            text_y = max(box[1]-font_size, 0)
+            im_draw.text((text_x, text_y), cat_name + score_str, fill='red', font=font)
         rel_out_image_path = image['file_name'] if preserve_files_tree else os.path.basename(image['file_name'])
         out_image_path = os.path.join(out_folder, rel_out_image_path)
         out_image_path = uniquer.unique(out_image_path)
@@ -131,6 +155,6 @@ if __name__ == '__main__':
             images = json.load(f)
             json_dict = {'images': images['images'], 'annotations': json_dict, 'categories': images['categories']}
     draw_boxes(json_dict, args.images_folder, args.out_folder,
-               draw_single_image=args.draw_single_image, images_number=args.images_number, random=args.random,
-               preserve_files_tree=args.preserve_files_tree, threshold=args.threshold)
+               images_files_to_draw=args.images_files_to_draw, images_number=args.images_number, random=args.random,
+               only_with_boxes=args.only_with_boxes, preserve_files_tree=args.preserve_files_tree, threshold=args.threshold)
 
