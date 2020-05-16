ba9dfcf1774c558a9bfba9a3f852cdd4b88cd30e
andrey1908 <andrey.krishtopik@mail.ru>
2020-05-16 12:03:33 +0300

    slight changes

--- cvat2coco.py
+++ cvat2coco.py
@@ -62,50 +62,51 @@ def cvat_root2coco_dict(root, detections_only=False, images_info=False, shortene
 
     # get categories
     labels = root.find('meta').find('task').find('labels')
-    class_to_idx = {}
-    idx = 1
+    category_name_to_id = {}
+    cat_id = 1
     for label in labels:
-        category = {"id": idx, "name": label[0].text}
+        category = {"id": cat_id, "name": label[0].text}
         categories.append(category)
-        class_to_idx[label.find('name').text] = idx
-        idx += 1
+        category_name_to_id[label.find('name').text] = cat_id
+        cat_id += 1
 
     # get images and annotations
-    idx = 1
-    for image in root.findall('image'):
-        image.attrib["id"] = int(image.attrib["id"])
-        image.attrib["height"] = int(image.attrib["height"])
-        image.attrib["width"] = int(image.attrib["width"])
-        image.attrib["file_name"] = image.attrib.pop("name")
-        images.append(image.attrib)
-        for box in image.findall('box'):
+    ann_id = 1
+    for cvat_image in root.findall('image'):
+        image = dict()
+        image["id"] = int(cvat_image.attrib["id"])
+        image["height"] = int(cvat_image.attrib["height"])
+        image["width"] = int(cvat_image.attrib["width"])
+        image["file_name"] = cvat_image.attrib.pop("name")
+        images.append(image)
+        for box in cvat_image.findall('box'):
             annotation = dict()
             bbox = [float(box.attrib["xtl"]), float(box.attrib["ytl"]),
                     float(box.attrib["xbr"]) - float(box.attrib["xtl"]),
                     float(box.attrib["ybr"]) - float(box.attrib["ytl"])]
             annotation["area"] = bbox[2] * bbox[3]
             annotation["iscrowd"] = 0
-            annotation["image_id"] = int(image.attrib["id"])
+            annotation["image_id"] = int(cvat_image.attrib["id"])
             annotation["bbox"] = bbox
-            annotation["category_id"] = class_to_idx[box.attrib["label"]]
-            annotation["id"] = idx
+            annotation["category_id"] = category_name_to_id[box.attrib["label"]]
+            annotation["id"] = ann_id
             if "score" in box.attrib.keys():
                 annotation["score"] = float(box.attrib["score"])
             annotations.append(annotation)
-            idx += 1
-        for polygon in image.findall('polygon'):
+            ann_id += 1
+        for polygon in cvat_image.findall('polygon'):
             annotation = dict()
             annotation["iscrowd"] = 0
-            annotation["image_id"] = int(image.attrib["id"])
+            annotation["image_id"] = int(cvat_image.attrib["id"])
             annotation["segmentation"] = cvat_segmentation_to_coco(polygon.attrib["points"])
             annotation["bbox"] = segmentation_to_bbox(annotation["segmentation"])
             annotation["area"] = annotation["bbox"][2] * annotation["bbox"][3]
-            annotation["category_id"] = class_to_idx[polygon.attrib["label"]]
-            annotation["id"] = idx
+            annotation["category_id"] = category_name_to_id[polygon.attrib["label"]]
+            annotation["id"] = ann_id
             if "score" in polygon.attrib.keys():
                 annotation["score"] = float(polygon.attrib["score"])
             annotations.append(annotation)
-            idx += 1
+            ann_id += 1
 
     if shortened_file_names:
         images = shorten_file_names(images)
